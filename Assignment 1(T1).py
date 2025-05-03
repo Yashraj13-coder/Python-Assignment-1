93ab28a2cd1edde5369631e0f93d2d63ea4f49eb
Yashraj13-coder <yashrajkhoje3@gmail.com>
2025-05-04 02:46:28 +0530

    Add files via upload

---
+++ Assignment 1(T1).py
@@ -0,0 +1,19 @@
+#Task 1: Perform Basic Mathematical Operations
+print('Hello, Here we perform basic mathematical operations')
+a=input('The first input value is: ')
+print(a)
+b=input('The second input value is: ')
+print(b)
+a=float(a)
+b=float(b)
+p=a+b
+print('Addition: ',p)
+q=a-b
+print('Substraction: ',q)
+r=a*b
+print('Multiplication: ',r)
+s=a/b
+print('Division: ',s)
+print('Thank You!')
+
+
---
+++ Assignment 1(T2).py
@@ -0,0 +1,7 @@
+#Task 2: Create a Personalized Greeting
+print('Hello, Here we are creating your personalized greeting')
+a=input('Enter your first name: ')
+print(a)
+b=input('Enter your last name: ')
+print(b)
+print('Hello, '+ a +' '+ b +'! Welcome to Python Programming.\nWe are excited to have you explore and contribute.\nHave a Great Learning.\nThank You!')
