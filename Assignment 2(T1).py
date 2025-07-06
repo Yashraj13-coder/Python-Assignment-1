e3c352ab1d7773a3901fd256b6bfebf5f782e8f5
Yashraj13-coder <yashrajkhoje3@gmail.com>
2025-07-06 08:20:34 +0530

    Add files via upload

---
+++ Assignment 2(T1).py
@@ -0,0 +1,6 @@
+#Program to Check if a number is even or odd
+a=int(input("Enter your number: "))
+if(a%2==0):
+    print(a,"is an Even Number.")
+else:
+    print(a,"is an Odd Number.")
---
+++ Assignment 2(T2).py
@@ -0,0 +1,5 @@
+#Program to print the sum of integer from 1 to 50 using a loop
+l=0
+for i in range(1,51):
+    l += i
+print("Sum of the integer from 1 to 50 is: ",l)
